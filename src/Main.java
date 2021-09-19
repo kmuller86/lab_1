cb6ca1b03834d778b9bd8b1630cdefda111c4c10
makarth <maciejczyk.art@gmail.com>
2021-09-19 12:54:21 +0200

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        double liczbaA = 34, liczbaB = 12, liczbaC = 2;
+        boolean boolA = false, boolB = true, boolC = false;
+        System.out.println((liczbaA + liczbaB) * liczbaC / liczbaA);
+        System.out.println((liczbaA - liczbaB * liczbaC / liczbaA) % 2);
 
+        System.out.println((boolA || boolB) && (boolB || boolC));
+        System.out.println((boolA || !boolB) || !(boolA && boolC));
+        System.out.println((boolA || boolB) && (boolB || boolC) && !(liczbaA > liczbaB) || !(liczbaC == liczbaA));
     }
 }
