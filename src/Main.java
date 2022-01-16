27e829110782e19ad9ed03535560df46c141bc00
kmuller86 <89707079+kmuller86@users.noreply.github.com>
2022-01-17 00:08:23 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
         double[] tab = new double[5];
+        // powinno byc i++, jest drobna różnica między tymi operatorami ;) ogólnie jest ok
         for(int i = 0; i < tab.length; ++i) {
             tab[i] = i + 0.34;
             System.out.printf("%f ", tab[i]);
